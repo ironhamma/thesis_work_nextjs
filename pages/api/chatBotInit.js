174a5904ce8c0623d7e9898a2db6b7ac213cc434
Szabo Benedek <benedek.szabo4@gmail.com>
2020-10-24 16:40:44 +0200

    FM valdation try 2

--- pages/api/chatBotInit.js
+++ pages/api/chatBotInit.js
@@ -1,7 +1,7 @@
 export default async(req, res) => {
     console.log(req.query);
         if(req.query['hub.verify_token'] === "szabobeno") {
-            res.send(req.query['hub.challenge']);
+            await res.send(req.query['hub.challenge']);
         }
-        res.send('Wrong token');
+        await res.send('Wrong token');
 }
