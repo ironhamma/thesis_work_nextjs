9bc9d1ab7dafaa2b1c73b173ff219300c4f8cd7d
Szabo Benedek <benedek.szabo4@gmail.com>
2020-10-25 03:24:47 +0200

    How about now?

--- pages/api/chatBotInit.js
+++ pages/api/chatBotInit.js
@@ -23,7 +23,7 @@ export default (req, res) => {
     const body = req.body;
 
     if (body.object === "page") {
-      body.entry.forEach(function (entry) {
+      body.entry.forEach(function async (entry) {
         // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
@@ -35,9 +35,9 @@ export default (req, res) => {
         // Check if the event is a message or postback and
         // pass the event to the appropriate handler function
         if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
+        await handleMessage(sender_psid, webhook_event.message);
         } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback);
+        await handlePostback(sender_psid, webhook_event.postback);
         }
       });
 
